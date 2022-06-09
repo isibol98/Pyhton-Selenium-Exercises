4e152e9f4ebecf39d383150ebb2a868ca0861c5f
isibol98 <ilkkanisibol@gmail.com>
2022-06-09 15:46:58 +0300

    get instagram followers list

---
+++ instagram-FollowerList.py
@@ -0,0 +1,39 @@
+from instagramUserInfo import username, password
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+class Instagram:
+    def __init__(self, username, password):
+        self.browser = webdriver.Chrome()
+        self.username = username
+        self.password = password
+        self.followers = []
+    def signIn (self):
+        self.browser.get("https://www.instagram.com/")
+        time.sleep(2)
+        usernameInput = self.browser.find_element(By.XPATH, "//*[@id='loginForm']/div/div[1]/div/label/input")
+        passwordInput = self.browser.find_element(By.XPATH, "//*[@id='loginForm']/div/div[2]/div/label/input")
+        
+        usernameInput.send_keys(self.username)
+        time.sleep(1)
+        passwordInput.send_keys(self.password)
+        time.sleep(1)
+        passwordInput.send_keys(Keys.ENTER)
+        time.sleep(3)
+
+    def getFollowers (self):
+        self.browser.get(f"https://www.instagram.com/{username}/followers/")
+        time.sleep(3)
+        followerList = self.browser.find_element(By.CSS_SELECTOR, "div[role=dialog] ul").find_elements(By.CSS_SELECTOR, "li")
+        for user in followerList:
+            link = user.find_element(By.CSS_SELECTOR, "a").get_attribute("href")
+            print(link)
+
+
+
+instagram = Instagram(username, password)
+instagram.signIn()
+instagram.getFollowers()
